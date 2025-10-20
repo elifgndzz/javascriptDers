a1fa0d6393ac17f98c559e5552457198a7cd3e14
Elif Gündüz <elifece3452@gmail.com>
2025-10-20 21:17:46 +0300

    çıktı verme işlemi eklendi

---
+++ ciktiVermeİslemleri.js
@@ -0,0 +1,6 @@
+console.log("Elif");
+console.log("Gündüz");
+console.log("3,55");
+console.info("Bu Bir Bilgi Mesajıdır");
+console.warn("BU BİR UYARI MESAJIDIR");
+console.error("BU BİR UYARI MESAJIDIR!!");
