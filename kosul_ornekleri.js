1d3dfc4b6f9e3bbd8a0d776f2870e9b6cb52b586
Elif Gündüz <elifece3452@gmail.com>
2025-11-06 21:43:36 +0300

    Add files via upload

---
+++ kosul_ornekleri.js
@@ -0,0 +1,16 @@
+let not = 85;
+if (not >= 90)
+{
+  console.log("Harf notu: A");
+}
+else if (not >= 80) {
+  console.log("Harf notu: B");
+}
+else if (not >= 70) {
+  console.log("Harf notu: C");
+}
+else
+     {
+  console.log("Harf notu: F - Kaldınız");
+
+}
