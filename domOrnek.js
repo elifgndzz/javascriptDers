f296ee30e15812d642d4526c0090137c14441e33
Elif Gündüz <elifece3452@gmail.com>
2025-12-11 11:54:24 +0300

    Add files via upload

---
+++ domOrnek.js
@@ -0,0 +1,11 @@
+let ekran=document.querySelector("#yazi");
+let buton=document.querySelector("#buton");
+
+//olay dinleyicisi ekle
+buton.addEventListener(`click`,function()
+    {
+        ekran.textContent="İşlem Tamamlandı!";
+        ekran.style.color="blue";
+        buton.style.display="none";
+    }
+)
